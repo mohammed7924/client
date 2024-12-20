be8629baeed527fe22a1a85f4f3d3559c65d0435
mohammed7924 <mohammedparachikottil@gmail.com>
2024-12-20 20:00:16 +0530

    client S3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
-}
+export class AppComponent implements OnInit{
+
+  title = 'DatinApp';
+  http=inject(HttpClient);
+users:any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next:response=>this.users=response,
+      error:error=>console.log(console.error()),
+      complete: () =>console.log('request has completed')
+
+  });
+
+
+
+
+    }
+  }
+
